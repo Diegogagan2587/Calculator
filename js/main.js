ef270d35d6ecf38cd2c41bd5f0ed251848740124
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-04-18 11:43:18 -0600

    Add Event Listenet to calculator buttons,return a console log(e)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log('javascript file is working properly');
+
+const calcButtonsContainer = document.getElementById('CalcButtonsContainer');
+calcButtonsContainer.addEventListener('click',addElementsToScreen);
+
+function addElementsToScreen(e){
+    console.log(e.target);
+}
