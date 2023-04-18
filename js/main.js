77f00165ca602a7f1ab6cae78f3e003f38206b6a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-04-18 12:23:02 -0600

    Add functiion to show the buttons content into the calculator screen, remove screen placeholder

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ calcButtonsContainer.addEventListener('click',addElementsToScreen);
 
 function addElementsToScreen(e){
     console.log(e.target);
+    let screenText = document.getElementById('screenCalculator').querySelector('p').innerText;
+    let buttonText = e.target.innerText;
+    console.log(screenText,buttonText);
+    screenText += buttonText;
+    return document.getElementById('screenCalculator').querySelector('p').innerText = screenText;
 }
