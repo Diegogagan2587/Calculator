738a452c08fff630bee133e40d3bb05038972834
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-04-18 18:44:00 -0600

    Add sum feature and reduccion feature using new getCalculatedResult function

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const calcButtonsContainer = document.getElementById('CalcButtonsContainer');
 calcButtonsContainer.addEventListener('click',addElementsToScreen);
 
 function addElementsToScreen(e){
-    console.log(e.target);
+    console.log(e.target);//shows button pressed;
     let screenText = document.getElementById('screenCalculator').querySelector('p').innerText;
     let buttonText = e.target.innerText;
     console.log(screenText,buttonText);
@@ -20,5 +20,33 @@ function addElementsToScreen(e){
         screenText = screenText.slice(0,-1);
         console.log('after removing last element=',screenText);
     }
+    if(buttonText == '='){
+        screenText = getCalculatedResult(screenText);
+    }
     return document.getElementById('screenCalculator').querySelector('p').innerText = screenText;
+}
+
+//helper functions below;
+function getCalculatedResult(str){
+    let result;
+    let arrFromStr = str.split(/[^0-9]/);
+    let operand = str.split(/[0-9]/);
+    console.log('running getResult(',str,')');
+    operand = operand.filter((element)=>{
+        if(element && element!= '='){
+            return element;
+        }
+    })
+    arrFromStr = arrFromStr.filter((element)=>{
+        if(element) {
+            return element;
+        }
+    })
+    if(operand == '+'){
+        result = (+arrFromStr[0])+(+arrFromStr[1]);
+    } else if(operand == '-'){
+        result = (+arrFromStr[0])-(+arrFromStr[1]);
+    }
+    console.log('result = ',result);
+    return result;
 }
